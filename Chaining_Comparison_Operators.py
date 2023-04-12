1ae88ee206641a0a665ce4301d8a83aae6c20c1e
1994Khushi <ayangelyoung@gmai.com>
2023-04-12 19:21:07 +0530

    chaining_operator

---
+++ Chaining_Comparison_Operators.py
@@ -0,0 +1,20 @@
+a =1<2
+print(a)
+b= 2<3
+print(b)
+c =1<2>3
+print(c)
+d= 1<2 and 2>3
+print(d)
+e= 'h' == 'h' and 2 == 2
+print(e)
+f = 1==1 or 2==3
+print(f)
+g =100 ==1 or 200==2
+print(g)
+h = 1==1
+print(h)
+h1= not 1==1
+print(h1)
+i= not 400>5000
+print(i)
