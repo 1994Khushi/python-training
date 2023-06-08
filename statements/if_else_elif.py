063a023c6e25cbfa2cb18c18f82eed3e39a9d0b6
1994Khushi <ayangelyoung@gmai.com>
2023-06-08 10:38:16 +0530

    statements

---
+++ statements/if_else_elif.py
@@ -0,0 +1,37 @@
+         #todo IF STATEMENT
+if True:
+    print('its TRUE!')
+
+if 3>2:
+    print("true")
+
+hungry = True
+if hungry:
+    print('feed me!')
+
+         # TODO IF-ELSE STATEMENT
+hungry = False
+if hungry:
+    print('feed me!')
+else:
+    print('I m not hungry')
+
+         #TODO ELIF STATEMENT
+loc = 'Store'
+if loc == 'Auto Shop' :
+    print("Cars are cool !")
+elif loc == 'Bank':
+    print("Money is cool !")
+elif loc == 'Store':
+    print("Welcome to the store !")
+else:
+    print("I do not know much. ")
+
+name = 'Sunny'
+if name == 'khushi':
+    print('Hello khushi!')
+elif name == 'Sunny':
+    print("Hello sunny!")
+else:
+    print("What is your name !")
+    
